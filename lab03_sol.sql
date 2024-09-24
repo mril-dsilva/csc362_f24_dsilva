2cd0c70727911cf62abbc790e193759a400ef6d4
Mril D'silva <mril.dsilva@centre.edu>
2024-09-24 22:34:33 +0000

    Ceated Appropriate tables and fields. Added comments

--- lab03_sol.sql
+++ lab03_sol.sql
@@ -56,9 +56,10 @@ VALUES (1,1,'2010-09-02 10:54:19',4),
 SHOW CREATE TABLE Movies;
 SHOW CREATE TABLE Consumers;
 
-SELECT * FROM Consumers;
-SELECT * FROM Movies;
+SELECT * FROM Consumers; /*View all consumer data*/
+SELECT * FROM Movies; /*View all Movie data*/
 
+/*Join Ratings, Consumers and Movies to see the rating list*/
 SELECT consumer_first_name, consumer_last_name, movie_title, rating_star_number
       FROM Movies
           NATURAL JOIN Ratings
@@ -75,6 +76,7 @@ DROP DATABASE IF EXISTS movie_ratings;
 CREATE DATABASE movie_ratings;
 USE movie_ratings;
 
+/*Initializing Tables needed and their fields*/
 CREATE TABLE Movies (
     PRIMARY KEY (movie_id), /* Setting the Primary Key */
     movie_id                  INT AUTO_INCREMENT,
@@ -113,12 +115,14 @@ CREATE TABLE Ratings (
     rating_star_number        INT
 );
 
+/*Adding data into Movies table*/
 INSERT INTO Movies (movie_title,movie_release_date)
 VALUES ("The Hunt for Red October",'1990-03-02'),
        ("Lady Bird",'2017-12-01'),
        ("Inception",'2010-08-16'),
        ("Monty Python and the Holy Grail",'1975-04-03');
 
+/*Adding data into Genres table*/
 INSERT INTO Genres (genre_name)
 VALUES ("Action"),
        ("Adventure"),
@@ -127,6 +131,7 @@ VALUES ("Action"),
        ("Drama"),
        ("Comedy");
 
+/*Adding data into Movie Genres table*/
 INSERT INTO Movie_Genres (movie_id,genre_id)
 VALUES (1,1),
        (1,2),
@@ -138,12 +143,14 @@ VALUES (1,1),
        (3,4),
        (4,6);
 
+/*Adding data into Consumers table*/
 INSERT INTO Consumers (consumer_first_name,consumer_last_name,consumer_address,consumer_city,consumer_state,consumer_zip_code)
 VALUES ("Toru","Okada","800 Glenridge Ave","Hobart","IN","46343"),
        ("Kumiko","Okada","864 NW Bohemia St","Vincentown","NJ","08088"),
        ("Noboru","Wataya","342 Joy Ridge St","Hermitage","TN","37076"),
        ("May","Kasahara","5 Kent Rd","East Haven","CT","06512");
 
+/*Adding data into Ratings table*/
 INSERT INTO Ratings (movie_id,consumer_id,rating_when_rated,rating_star_number)
 VALUES (1,1,'2010-09-02 10:54:19',4),
        (1,3,'2012-08-05 15:00:01',3),
@@ -151,7 +158,7 @@ VALUES (1,1,'2010-09-02 10:54:19',4),
        (2,3,'2017-03-27 00:12:48',2),
        (2,4,'2018-08-02 00:54:42',4);
 
-/*Joining the two to view*/
+/*Joining the two to view the many to many relationship*/
 
 SELECT movie_title, genre_name 
     FROM Movies
